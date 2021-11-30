ccb666c0241db32b485bc45afc53d00bdd7dd1df
Astrid Hsu <wellbeing81903@gmail.com>
2021-11-30 15:34:33 +0800

    Add MathClass

--- AppSample/src/RedVelvet/wifeSeulgi.java
+++ AppSample/src/RedVelvet/wifeSeulgi.java
@@ -3,5 +3,13 @@ package RedVelvet;
 public class wifeSeulgi {
 	public static void main(String[] args) {
 		System.out.println("3rd version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 5));
+	}
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
 	}
 }
